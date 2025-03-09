e54da95af8c5c665f4e37136495eefc41e85fc40
Marcelo Rufino <marcelorufinomaria@hotmail.com>
2025-03-09 10:49:36 -0300

    projeto criado

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Digite o número de sua agência:");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o número da sua conta:");
+        int conta = scanner.nextInt();
+
+        scanner.nextLine();
+        System.out.println("Digite seu nome:");
+        String nomeDoCliente = scanner.nextLine();
+
+        System.out.println("Digite o seu saldo:");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeDoCliente + ", " + "obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + " e seu saldo de " + saldo + " já está disponível para saque.");
     }
 }
